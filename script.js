194f6ba5fb2ba42b65c47a09ba25ffd06160d2b5
Enzo Jaques <enzojaques@icloud.com>
2023-07-01 16:06:09 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var more = document.getElementById("more")
+var logoIMG = document.getElementById("logoIMG")
+var logo = document.getElementById("logo")
+var search = document.getElementById("search")
+var lupa = document.getElementById("lupa")
+var profile = document.getElementById("profile")
+var notifications = document.getElementById("notifications")
+
+
+
+
+document.addEventListener("click", myFunction);
+
+function myFunction() {
+  window.alert("Sorry, this project focus on showing the Front End part of a website, therefore I focused on the HTML and CSS and won't be focusing on the JavaScript side of the website. Thank you very much for your attention.")
+}
